d18c263ead3b93809f3cb38cfbc781c913037260
RoyHinkley <jim.wilson@aeonlaboratories.com>
2024-03-26 19:48:00 -0700

    Updated csproj files for consistency. Prevented overwriting of settings.json and Credentials.json from Installation folder on Rebuild. Updated 'update code.cmd' to remove old .NET version.

--- Installation/update code.cmd
+++ Installation/update code.cmd
@@ -1,7 +1,7 @@
 @echo off
 C:
 CD \Programs\AeonLaboratories\Cegs12X
-if [%1]==[] %0 C:\Data\Source\DevStudio\Cegs12X\bin\Release\netcoreapp3.1
+if [%1]==[] %0 C:\Data\Source\DevStudio\Cegs12X\bin\Release
 copy "%*\*.exe" > nul
 copy "%*\*.dll" > nul
 copy "%*\*.config" > nul
